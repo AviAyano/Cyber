ecbc83c6b0e4b7e9a897afc765fb501574e1ea09
AviAyano <73743798+AviAyano@users.noreply.github.com>
2021-08-11 01:51:10 +0300

    Delete Factorial.py

--- Factorial.py
+++
@@ -1,6 +0,0 @@
-def factorial(sum,num):
-    for i in range(1, num + 1):
-        sum = i * sum
-    return sum
-if __name__ == '__main__':
-    print(factorial(1,int(input('Enter a integer number please: '))))
